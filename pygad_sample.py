13f6afe160e2ffa7389e8036e3e0dc54e081176e
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-03-23 17:00:37 -0400

    Taylor series guessing for sin(x) using PyGAD. It's pretty damn fast

--- pygad_sample.py
+++ pygad_sample.py
@@ -1,38 +1,51 @@
 import pygad
-import numpy
-
-function_inputs = [4, -2, 3.5, 5, -11, -4.7]
-desired_output = 44
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+# The sine function we want to match
+def target_function(x):
+    return np.sin(x)
+
+
+# Polynomial function of the form ax^3 + bx^2 + cx + d
+def polynomial_function(x, coefficients):
+    a, b, c, d = coefficients
+    return a * (x ** 3) + b * (x ** 2) + c * x + d
+
+
+# Fitness function to evaluate how well the polynomial matches sin(x)
 def fitness_func(ga_instance, solution, solution_idx):
-    output = numpy.sum(solution*function_inputs)
-    fitness = 1.0 / numpy.abs(output - desired_output)
+    # We will evaluate the fitness over the range of x from 0 to 2*pi
+    x_values = np.linspace(-np.pi, np.pi, 100)  # Generate 100 values of x from -pi to pi
+    target_values = target_function(x_values)  # Actual sin(x) values
+    predicted_values = polynomial_function(x_values, solution)  # Polynomial values
+
+    # Compute the squared error between the polynomial and the target function (sin(x))
+    error = np.sum((predicted_values - target_values) ** 2)  # Squared error is commonly used for regression tasks
+    fitness = 1.0 / (1.0 + error)  # We return the inverse of error, so the fitness is higher when error is smaller
     return fitness
 
 
-fitness_function = fitness_func
-
-num_generations = 50
+# Genetic algorithm parameters
+num_generations = 1000
 num_parents_mating = 4
-
 sol_per_pop = 8
-num_genes = len(function_inputs)
-
-init_range_low = -2
+num_genes = 4  # a, b, c, d are the 4 coefficients of the polynomial
+init_range_low = -5  # Coefficients can range from -5 to 5
 init_range_high = 5
 
+# Define the parent selection, crossover, and mutation methods
 parent_selection_type = "sss"
 keep_parents = 1
-
 crossover_type = "single_point"
-
 mutation_type = "random"
 mutation_percent_genes = 10
 
+# Initialize the genetic algorithm instance
 ga_instance = pygad.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_function,
+                       fitness_func=fitness_func,
                        sol_per_pop=sol_per_pop,
                        num_genes=num_genes,
                        init_range_low=init_range_low,
@@ -43,11 +56,29 @@ ga_instance = pygad.GA(num_generations=num_generations,
                        mutation_type=mutation_type,
                        mutation_percent_genes=mutation_percent_genes)
 
+# Run the genetic algorithm
 ga_instance.run()
 
+# Get the best solution found by the GA
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Parameters of the best solution : {solution}".format(solution=solution))
-print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
-prediction = numpy.sum(numpy.array(function_inputs)*solution)
-print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+# Print out the best solution (the coefficients of the polynomial)
+print("Best polynomial coefficients: a = {0}, b = {1}, c = {2}, d = {3}".format(solution[0], solution[1], solution[2],
+                                                                                solution[3]))
+print("Fitness value of the best solution = {0}".format(solution_fitness))
+
+# Test the polynomial with the best coefficients and compare to sin(x)
+x_values = np.linspace(-2* np.pi, 2 * np.pi, 100)
+predicted_values = polynomial_function(x_values, solution)
+target_values = target_function(x_values)
+
+# Plot the results
+plt.xlim(-5, 5)
+plt.ylim(-1.5, 1.5)
+plt.plot(x_values, target_values, label='sin(x)', color='blue')
+plt.plot(x_values, predicted_values, label='Polynomial', color='red', linestyle='dashed')
+plt.legend()
+plt.title('Polynomial vs. sin(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()

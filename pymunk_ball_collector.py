3639025ea3cddbe67c0f378376d4e5d38bfd482f
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-07 16:17:46 -0400

    cooking

--- pymunk_ball_collector.py
+++ pymunk_ball_collector.py
@@ -32,6 +32,10 @@ def simulate_balls(ga_instance, solution, solution_idx, *args):
     space = pymunk.Space()
     space.gravity = (0, 900)  # Gravity pulling down
 
+    # Create ground
+    floor = pymunk.Segment(space.static_body, (0, height - 50), (width, height - 50), 5)
+    floor.elasticity = 0
+    space.add(floor)
 
     # === Create Ball ===
     ball_mass, ball_radius = 5, 20

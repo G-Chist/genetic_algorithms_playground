290af08193264fb88cae5426eb09c3f77e9a6c51
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-07 20:57:19 -0400

    Better sample box

--- pymunk_ball_collector.py
+++ pymunk_ball_collector.py
@@ -93,8 +93,8 @@ def simulate_balls(ga_instance, solution, solution_idx, *args):
     # Create static components (collector)
     static_body = space.static_body
     static_lines_collector = [
-        pymunk.Segment(static_body, (500, height - 70), (580, height - 120), 5),
-        pymunk.Segment(static_body, (580, height - 120), (580, height - 70), 5),
+        pymunk.Segment(static_body, (500, height - 70), (580, height - 180), 5),
+        pymunk.Segment(static_body, (580, height - 180), (580, height - 70), 5),
         pymunk.Segment(static_body, (580, height - 70), (880, height - 70), 5),
         pymunk.Segment(static_body, (880, height - 70), (880, height - 300), 5),
         pymunk.Segment(static_body, (880, height - 300), (550, height - 300), 5),
